da95f11a05d6948f953059577f08468edc456eaf
Yifeng Li <charlesliyifeng@gmail.com>
2024-01-05 23:29:40 +1300

    add password validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,13 @@
 class User < ApplicationRecord
   has_secure_password
+  PASSWORD_REQUIREMENTS = /\A
+    (?=.{8,}) # at least 8 chars long
+    (?=.*\d) # contain at least one number
+    (?=.*[a-z]) # contain at least one lowercase letter
+    (?=.*[A-Z]) # contain at least one uppercase letter
+  /x
   validates :email, presence: true, uniqueness: true
+  validates :password, format: PASSWORD_REQUIREMENTS
 
   has_many :questions, dependent: :nullify
   has_many :answers, dependent: :nullify
