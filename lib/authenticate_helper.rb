f3401f3fb8433027926b9b6d5ce4c6b90c1d2eec
Yifeng Li <charlesliyifeng@gmail.com>
2024-01-18 22:02:41 +0800

    fix bug

--- lib/authenticate_helper.rb
+++ lib/authenticate_helper.rb
@@ -6,7 +6,7 @@ module AuthenticateHelper
 
   def load_current_user
     begin
-      email = JsonWebTokenService.decode(request.headers['HTTP_AUTH_TOKEN'])["email"]
+      email = JsonWebTokenService.decode(request.headers['HTTP_X_AUTH_TOKEN'])["email"]
       @current_user = User.find_by(email: email)
     rescue JWT::DecodeError
       puts "############################## no auth_token ####################################"
