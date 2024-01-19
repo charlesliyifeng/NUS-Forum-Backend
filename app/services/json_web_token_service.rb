876fd484ac0bb0a075129ea091464cb1ca6b6bbe
Yifeng Li <charlesliyifeng@gmail.com>
2024-01-19 10:36:28 +0800

    fix secrets

--- app/services/json_web_token_service.rb
+++ app/services/json_web_token_service.rb
@@ -1,9 +1,9 @@
 class JsonWebTokenService
   def self.encode(payload, exp = 24.hours.from_now)
-    JWT.encode payload, Rails.application.secrets.secret_key_base, 'HS256'
+    JWT.encode payload, Rails.application.secret_key_base, 'HS256'
   end
 
   def self.decode(token)
-    JWT.decode( token, Rails.application.secrets.secret_key_base, algorithm: 'HS256' )[0]
+    JWT.decode( token, Rails.application.secret_key_base, algorithm: 'HS256' )[0]
   end
 end
