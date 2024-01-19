d860badbf187c0fd3903de06467abc2630703f60
Yifeng Li <charlesliyifeng@gmail.com>
2024-01-19 10:40:38 +0800

    fix secret

--- app/services/json_web_token_service.rb
+++ app/services/json_web_token_service.rb
@@ -1,9 +1,10 @@
 class JsonWebTokenService
+  SECRET = Rails.application.secret_key_base
   def self.encode(payload, exp = 24.hours.from_now)
-    JWT.encode payload, Rails.application.secret_key_base, 'HS256'
+    JWT.encode( payload, SECRET, 'HS256' )
   end
 
   def self.decode(token)
-    JWT.decode( token, Rails.application.secret_key_base, algorithm: 'HS256' )[0]
+    JWT.decode( token, SECRET, algorithm: 'HS256' )[0]
   end
 end
