8095e9ded9ffe73cc6d21bb4b808e700b382ef62
Yifeng Li <charlesliyifeng@gmail.com>
2024-01-19 11:10:39 +0800

    test

--- app/services/json_web_token_service.rb
+++ app/services/json_web_token_service.rb
@@ -1,5 +1,5 @@
 class JsonWebTokenService
-  SECRET = "1s#$^aJS1231D76873^&*^Q$oadjhADHhaKJ91(&*839hduiwefvnmAOHgfiuqwb786340sl)&^$ad148"
+  SECRET = "helloworld123"
   def self.encode(payload, exp = 24.hours.from_now)
     JWT.encode( payload, SECRET, 'HS256' )
   end
